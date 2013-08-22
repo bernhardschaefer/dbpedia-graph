6885b2bf7577e8c2f9514a52236a3d17ab653a59
Bernhard Schäfer <bschaefer87@gmx.net>
2013-08-22 15:25:56 +0000

    small refactoring

--- src/main/java/de/unima/dws/dbpediagraph/graphdb/GraphConfig.java
+++ src/main/java/de/unima/dws/dbpediagraph/graphdb/GraphConfig.java
@@ -1,8 +1,5 @@
 package de.unima.dws.dbpediagraph.graphdb;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -16,33 +13,21 @@ import com.tinkerpop.blueprints.Graph;
  * 
  */
 public class GraphConfig {
+	/**
+	 * Holder for the singleton.
+	 */
+	private static class Holder {
+		public static final GraphConfig INSTANCE = new GraphConfig();
+	}
+
 	public static final String URI_PROPERTY = "URI";
 	public static final String DBPEDIA_RESOURCE_URI = "http://dbpedia.org/resource/";
 	public static final String EDGE_LABEL = "pred";
+
 	private static final String GRAPH_PROPERTY_FILE = "graph.properties";
 
-	public static final Map<String, String> URI_TO_PREFIX;
-	static {
-		URI_TO_PREFIX = new LinkedHashMap<String, String>();
-
-		// subject and objects are all dbpedia resources
-		URI_TO_PREFIX.put(DBPEDIA_RESOURCE_URI, "dbr:");
-
-		// top predicates extracted from dumps
-		URI_TO_PREFIX.put("http://dbpedia.org/ontology/", "dbo:");
-		URI_TO_PREFIX.put("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "rdf:");
-		URI_TO_PREFIX.put("http://purl.org/dc/terms/subject", "dcterms:");
-		URI_TO_PREFIX.put("http://xmlns.com/foaf/0.1/", "foaf:");
-		URI_TO_PREFIX.put("http://www.w3.org/2004/02/skos/core#", "skos:");
-		URI_TO_PREFIX.put("http://www.w3.org/2003/01/geo/wgs84_pos#", "pos:");
-		URI_TO_PREFIX.put("http://purl.org/dc/elements/1.1/", "dc:");
-		URI_TO_PREFIX.put("http://www.georss.org/georss/point", "poi:");
-
-		// other prefixes from http://dbpedia.org/snorql/
-		URI_TO_PREFIX.put("http://www.w3.org/2002/07/owl#", "owl:");
-		URI_TO_PREFIX.put("http://www.w3.org/2001/XMLSchema#", "xsd:");
-		URI_TO_PREFIX.put("http://www.w3.org/2000/01/rdf-schema#", "rdfs:");
-		URI_TO_PREFIX.put("http://dbpedia.org/property/", "dbpedia2:");
+	public static GraphConfig getInstance() {
+		return Holder.INSTANCE;
 	}
 
 	/**
@@ -61,17 +46,6 @@ public class GraphConfig {
 
 	public Configuration getConfig() {
 		return config;
-	}
-
-	/**
-	 * Holder for the singleton.
-	 */
-	private static class Holder {
-		public static final GraphConfig INSTANCE = new GraphConfig();
-	}
-
-	public static GraphConfig getInstance() {
-		return Holder.INSTANCE;
 	}
 
 }
--- src/main/java/de/unima/dws/dbpediagraph/graphdb/UriShortener.java
+++ src/main/java/de/unima/dws/dbpediagraph/graphdb/UriShortener.java
@@ -1,5 +1,7 @@
 package de.unima.dws.dbpediagraph.graphdb;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import com.tinkerpop.blueprints.Graph;
@@ -12,12 +14,35 @@ import com.tinkerpop.blueprints.Graph;
  * 
  */
 public class UriShortener {
+	public static final Map<String, String> URI_TO_PREFIX;
+	static {
+		URI_TO_PREFIX = new LinkedHashMap<String, String>();
+
+		// subject and objects are all dbpedia resources
+		URI_TO_PREFIX.put(GraphConfig.DBPEDIA_RESOURCE_URI, "dbr:");
+
+		// top predicates extracted from dumps
+		URI_TO_PREFIX.put("http://dbpedia.org/ontology/", "dbo:");
+		URI_TO_PREFIX.put("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "rdf:");
+		URI_TO_PREFIX.put("http://purl.org/dc/terms/subject", "dcterms:");
+		URI_TO_PREFIX.put("http://xmlns.com/foaf/0.1/", "foaf:");
+		URI_TO_PREFIX.put("http://www.w3.org/2004/02/skos/core#", "skos:");
+		URI_TO_PREFIX.put("http://www.w3.org/2003/01/geo/wgs84_pos#", "pos:");
+		URI_TO_PREFIX.put("http://purl.org/dc/elements/1.1/", "dc:");
+		URI_TO_PREFIX.put("http://www.georss.org/georss/point", "poi:");
+
+		// other prefixes from http://dbpedia.org/snorql/
+		URI_TO_PREFIX.put("http://www.w3.org/2002/07/owl#", "owl:");
+		URI_TO_PREFIX.put("http://www.w3.org/2001/XMLSchema#", "xsd:");
+		URI_TO_PREFIX.put("http://www.w3.org/2000/01/rdf-schema#", "rdfs:");
+		URI_TO_PREFIX.put("http://dbpedia.org/property/", "dbpedia2:");
+	}
 
 	/**
 	 * Shorten the uri by applying prefixes.
 	 */
 	public static String shorten(String uri) {
-		for (Entry<String, String> e : GraphConfig.URI_TO_PREFIX.entrySet()) {
+		for (Entry<String, String> e : URI_TO_PREFIX.entrySet()) {
 			if (uri.contains(e.getKey())) {
 				// there should be at most one replacement
 				return uri.replace(e.getKey(), e.getValue());
@@ -30,7 +55,7 @@ public class UriShortener {
 	 * Unshorten the uri by replacing prefixes with their full equivalent.
 	 */
 	public static String unshorten(String uri) {
-		for (Entry<String, String> e : GraphConfig.URI_TO_PREFIX.entrySet()) {
+		for (Entry<String, String> e : URI_TO_PREFIX.entrySet()) {
 			if (uri.contains(e.getValue())) {
 				// there should be at most one replacement
 				return uri.replace(e.getValue(), e.getKey());
