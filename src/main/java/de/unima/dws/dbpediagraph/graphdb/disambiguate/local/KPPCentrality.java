9f7dd4f0000ddb54ae915de23667fe83d94ba586
Bernhard Schäfer <bschaefer87@gmx.net>
2013-08-30 15:16:34 +0000

    removed unused code

--- src/main/java/de/unima/dws/dbpediagraph/graphdb/disambiguate/local/KPPCentrality.java
+++ src/main/java/de/unima/dws/dbpediagraph/graphdb/disambiguate/local/KPPCentrality.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 
@@ -15,35 +14,25 @@ import de.unima.dws.dbpediagraph.graphdb.disambiguate.ConnectivityMeasure;
 import de.unima.dws.dbpediagraph.graphdb.disambiguate.LocalDisambiguator;
 import de.unima.dws.dbpediagraph.graphdb.disambiguate.WeightedSense;
 import de.unima.dws.dbpediagraph.graphdb.wrapper.GraphJungUndirected;
-import edu.uci.ics.jung.algorithms.scoring.DistanceCentralityScorer;
 import edu.uci.ics.jung.algorithms.shortestpath.Distance;
 import edu.uci.ics.jung.algorithms.shortestpath.UnweightedShortestPath;
-import edu.uci.ics.jung.graph.Hypergraph;
 
 public class KPPCentrality implements LocalDisambiguator {
-	static class KPPScorer extends DistanceCentralityScorer<Vertex, Edge> {
-		public KPPScorer(Hypergraph<Vertex, Edge> graph) {
-			super(graph, false);
-		}
-	}
 
 	@Override
 	public List<WeightedSense> disambiguate(Collection<String> senses, Graph subgraph) {
-		// ClosenessCentrality<Vertex, Edge>
-		Distance<Vertex> sp = new UnweightedShortestPath<>(new GraphJungUndirected(subgraph));
+		Distance<Vertex> distances = new UnweightedShortestPath<>(new GraphJungUndirected(subgraph));
 		int numberOfVertices = GraphUtil.getNumberOfVertices(subgraph);
 
-		// KPPScorer kpp = new KPPScorer(new GraphJung<Graph>(subgraph));
 		List<WeightedSense> weightedUris = new LinkedList<>();
 		for (String sense : senses) {
 			Vertex v = GraphUtil.getVertexByUri(subgraph, sense);
-			// double score = kpp.getVertexScore(v);
-			Map<Vertex, Number> distances = sp.getDistanceMap(v);
+			Map<Vertex, Number> distancesFromVertex = distances.getDistanceMap(v);
 			double sumInverseShortestDistances = 0;
 			for (Vertex otherVertex : subgraph.getVertices()) {
 				if (otherVertex.equals(v))
 					continue;
-				Number distance = distances.get(otherVertex);
+				Number distance = distancesFromVertex.get(otherVertex);
 				double inverseShortestDistance;
 				if (distance == null || distance.doubleValue() == 0)
 					// if there is no path, the distance is the constant 1/(numberOfVertices-1)
