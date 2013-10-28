1c54d5f1900abd9341e0378437128ab51ebb8736
Bernhard Schäfer <bschaefer87@gmx.net>
2013-10-28 15:42:22 +0000

    minor bug fixes

--- src/main/java/de/unima/dws/dbpediagraph/graphdb/disambiguate/AbstractLocalGraphDisambiguator.java
+++ src/main/java/de/unima/dws/dbpediagraph/graphdb/disambiguate/AbstractLocalGraphDisambiguator.java
@@ -1,12 +1,19 @@
 package de.unima.dws.dbpediagraph.graphdb.disambiguate;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 
 import de.unima.dws.dbpediagraph.graphdb.Graphs;
-import de.unima.dws.dbpediagraph.graphdb.model.*;
+import de.unima.dws.dbpediagraph.graphdb.model.ModelFactory;
+import de.unima.dws.dbpediagraph.graphdb.model.ModelTransformer;
+import de.unima.dws.dbpediagraph.graphdb.model.Sense;
+import de.unima.dws.dbpediagraph.graphdb.model.SurfaceForm;
+import de.unima.dws.dbpediagraph.graphdb.model.SurfaceFormSenseScore;
+import de.unima.dws.dbpediagraph.graphdb.model.SurfaceFormSenses;
 import edu.uci.ics.jung.algorithms.scoring.VertexScorer;
 
 /**
@@ -31,7 +38,8 @@ public abstract class AbstractLocalGraphDisambiguator<T extends SurfaceForm, U e
 		VertexScorer<Vertex, Double> vertexScorer = getVertexScorer(subgraph);
 		List<SurfaceFormSenseScore<T, U>> senseScores = ModelTransformer.initializeScores(surfaceFormsSenses, factory);
 		for (SurfaceFormSenseScore<T, U> senseScore : senseScores) {
-			double score = vertexScorer.getVertexScore(Graphs.vertexByUri(subgraph, senseScore.sense().fullUri()));
+			Vertex v = Graphs.vertexByUri(subgraph, senseScore.sense().fullUri());
+			double score = (v == null) ? -1 : vertexScorer.getVertexScore(v);
 			senseScore.setScore(score);
 		}
 		Collections.sort(senseScores);
