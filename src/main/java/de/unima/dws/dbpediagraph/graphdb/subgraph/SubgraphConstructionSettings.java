53033181f2b36ceff83b619524ccf46917224ae8
Bernhard Schäfer <bschaefer87@gmx.net>
2013-11-11 15:46:22 +0000

    read subgraph construction settings from configuration

--- src/main/java/de/unima/dws/dbpediagraph/graphdb/subgraph/SubgraphConstructionSettings.java
+++ src/main/java/de/unima/dws/dbpediagraph/graphdb/subgraph/SubgraphConstructionSettings.java
@@ -1,5 +1,7 @@
 package de.unima.dws.dbpediagraph.graphdb.subgraph;
 
+import org.apache.commons.configuration.Configuration;
+
 import de.unima.dws.dbpediagraph.graphdb.GraphType;
 import de.unima.dws.dbpediagraph.graphdb.filter.DummyEdgeFilter;
 import de.unima.dws.dbpediagraph.graphdb.filter.EdgeFilter;
@@ -12,10 +14,71 @@ import de.unima.dws.dbpediagraph.graphdb.filter.EdgeFilter;
  * 
  */
 public final class SubgraphConstructionSettings {
+	// keys in configuration file
+	private static final String MAX_DISTANCE_KEY = "de.unima.dws.dbpediagraph.graphdb.subgraph.maxDistance";
+	private static final String GRAPH_TYPE_KEY = "de.unima.dws.dbpediagraph.graphdb.subgraph.graphType";
+	private static final String EXPLORATION_THRESHOLD_KEY = "de.unima.dws.dbpediagraph.graphdb.subgraph.explorationThreshold";
+	private static final String EDGE_FILTER_KEY = "de.unima.dws.dbpediagraph.graphdb.subgraph.edgeFilter";
+
+	/**
+	 * Instance with default settings
+	 */
+	private static final SubgraphConstructionSettings DEFAULT = new SubgraphConstructionSettings.Builder().build();
+
+	final ExplorationThreshold explorationThreshold;
+	final EdgeFilter edgeFilter;
+	public final GraphType graphType;
+	/**
+	 * "the distance between two vertices in a graph is the <i>number of edges</i> in a shortest path connecting them."
+	 * 
+	 * @see <a
+	 *      href="http://en.wikipedia.org/wiki/Distance_(graph_theory)">http://en.wikipedia.org/wiki/Distance_(graph_theory)</a>
+	 */
+	final int maxDistance;
+
+	private SubgraphConstructionSettings(Builder builder) {
+		this.explorationThreshold = builder.explorationThreshold;
+		this.edgeFilter = builder.edgeFilter;
+		this.graphType = builder.graphType;
+		this.maxDistance = builder.maxDistance;
+	}
+
+	/**
+	 * Get the instance with default settings
+	 */
+	public static SubgraphConstructionSettings getDefault() {
+		return DEFAULT;
+	}
+
+	public static SubgraphConstructionSettings fromConfig(Configuration config) {
+		SubgraphConstructionSettings.Builder builder = new SubgraphConstructionSettings.Builder();
+
+		Integer maxDistance = config.getInt(MAX_DISTANCE_KEY);
+		if (maxDistance != null)
+			builder.maxDistance(maxDistance);
+
+		String graphTypeString = config.getString(GRAPH_TYPE_KEY);
+		if (graphTypeString != null) {
+			GraphType graphType = GraphType.valueOf(graphTypeString);
+			builder.graphType(graphType);
+		}
+
+		String explorationThresholdClassName = config.getString(EXPLORATION_THRESHOLD_KEY);
+		if (explorationThresholdClassName != null) {
+			// TODO implement
+		}
+
+		String edgeFilterClassName = config.getString(EDGE_FILTER_KEY);
+		if (edgeFilterClassName != null) {
+			// TODO implement
+		}
+
+		return builder.build();
+	}
 
 	public static class Builder {
 		// parameters are initialized to default values
-		private final ExplorationThreshold explorationThreshold = DegreeThreshold.getDefault();
+		private ExplorationThreshold explorationThreshold = DegreeThreshold.getDefault();
 		private EdgeFilter edgeFilter = new DummyEdgeFilter();
 		private GraphType graphType = GraphType.DIRECTED_GRAPH;
 		private int maxDistance = 4;
@@ -38,30 +101,11 @@ public final class SubgraphConstructionSettings {
 			this.maxDistance = maxDistance;
 			return this;
 		}
-	}
 
-	private static final SubgraphConstructionSettings DEFAULT = new SubgraphConstructionSettings.Builder().build();
-
-	public static SubgraphConstructionSettings getDefault() {
-		return DEFAULT;
-	}
-
-	final ExplorationThreshold explorationThreshold;
-	final EdgeFilter edgeFilter;
-	public final GraphType graphType;
-	/**
-	 * "the distance between two vertices in a graph is the <i>number of edges</i> in a shortest path connecting them."
-	 * 
-	 * @see <a
-	 *      href="http://en.wikipedia.org/wiki/Distance_(graph_theory)">http://en.wikipedia.org/wiki/Distance_(graph_theory)</a>
-	 */
-	final int maxDistance;
-
-	private SubgraphConstructionSettings(Builder builder) {
-		this.explorationThreshold = builder.explorationThreshold;
-		this.edgeFilter = builder.edgeFilter;
-		this.graphType = builder.graphType;
-		this.maxDistance = builder.maxDistance;
+		public Builder explorationThreshold(ExplorationThreshold explorationThreshold) {
+			this.explorationThreshold = explorationThreshold;
+			return this;
+		}
 	}
 
 }
