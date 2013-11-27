0f161d96617d29b9bb69bc7ffbd56ad3c30a077c
Bernhard Schäfer <bschaefer87@gmx.net>
2013-11-27 11:46:46 +0000

    reduced visibility of package related methods

--- src/main/java/de/unima/dws/dbpediagraph/weights/GraphWeightsFactory.java
+++ src/main/java/de/unima/dws/dbpediagraph/weights/GraphWeightsFactory.java
@@ -37,17 +37,16 @@ public final class GraphWeightsFactory {
 		return DBpediaGraphWeightsHolder.GRAPH_WEIGHTS;
 	}
 
-	public static Map<String, Integer> newTransientMap() {
+	static Map<String, Integer> newTransientMap() {
 		return new Object2IntOpenHashMap<String>();
 	}
 
-	public static PersistentMap<String, Integer> newPersistentWeightsMap() {
+	static PersistentMap<String, Integer> newPersistentWeightsMap() {
 		boolean readOnly = false, clear = true;
 		return loadPersistentWeightsMap(GraphConfig.config(), clear, readOnly);
 	}
 
-	private static PersistentMap<String, Integer> loadPersistentWeightsMap(Configuration config, boolean clear,
-			boolean readOnly) {
+	static PersistentMap<String, Integer> loadPersistentWeightsMap(Configuration config, boolean clear, boolean readOnly) {
 		long startTime = System.currentTimeMillis();
 
 		String dbName = "all";
@@ -64,10 +63,10 @@ public final class GraphWeightsFactory {
 	public static void main(String[] args) {
 		boolean clear = false, readOnly = true;
 		PersistentMap<String, Integer> persistentMap = loadPersistentWeightsMap(GraphConfig.config(), clear, readOnly);
-		if (persistentMap instanceof BerkeleyDB) {
-			BerkeleyDB<String, Integer> db = (BerkeleyDB<String, Integer>) persistentMap;
-			BerkeleyDB.queryContent(db);
-		}
+		if (!(persistentMap instanceof BerkeleyDB))
+			throw new IllegalStateException("The loaded map is no berkeley db.");
+		BerkeleyDB<String, Integer> db = (BerkeleyDB<String, Integer>) persistentMap;
+		BerkeleyDB.queryContent(db);
 	}
 
 	// Suppress default constructor for noninstantiability
