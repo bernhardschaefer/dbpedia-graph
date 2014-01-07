a53ccb775a7722b4bf3acb783990bf76f59e5a7c
Bernhard Schäfer <bschaefer87@gmx.net>
2014-01-07 23:22:27 +0000

    Normalized Degree Centrality

---
+++ src/main/java/de/unima/dws/dbpediagraph/disambiguate/local/NormalizedDegreeCentrality.java
@@ -0,0 +1,46 @@
+package de.unima.dws.dbpediagraph.disambiguate.local;
+
+import com.tinkerpop.blueprints.*;
+
+import de.unima.dws.dbpediagraph.disambiguate.*;
+import de.unima.dws.dbpediagraph.graph.*;
+import de.unima.dws.dbpediagraph.graph.GraphFactory;
+import de.unima.dws.dbpediagraph.model.Sense;
+import de.unima.dws.dbpediagraph.model.SurfaceForm;
+import de.unima.dws.dbpediagraph.weights.EdgeWeights;
+import edu.uci.ics.jung.algorithms.scoring.VertexScorer;
+
+/**
+ * Normalized Degree Centrality {@link GraphDisambiguator} that uses the vertex degree in the subgraph normalized by the
+ * degree of the vertex in the global graph.
+ * 
+ * @author Bernhard Schäfer
+ * 
+ */
+public class NormalizedDegreeCentrality<T extends SurfaceForm, U extends Sense> extends
+		AbstractLocalGraphDisambiguator<T, U> implements LocalGraphDisambiguator<T, U> {
+
+	public NormalizedDegreeCentrality(GraphType graphType, EdgeWeights graphWeights, Boolean usePriorFallback) {
+		super(graphType, graphWeights, usePriorFallback);
+	}
+
+	class DegreeVertexScorer implements VertexScorer<Vertex, Double> {
+		@Override
+		public Double getVertexScore(Vertex v) {
+			// TODO should Direction.IN be used for directed graphs?
+			double subgraphDegree = Graphs.vertexDegreeWeighted(v, Direction.BOTH, edgeWeights);
+
+			Vertex vDBpediaGraph = GraphFactory.getDBpediaGraph().getVertex(v.getId());
+			double totalDegree = Graphs.vertexDegreeWeighted(vDBpediaGraph, Direction.BOTH, edgeWeights);
+
+			double centrality = subgraphDegree / totalDegree;
+			return centrality;
+		}
+
+	}
+
+	@Override
+	protected VertexScorer<Vertex, Double> getVertexScorer(Graph subgraph) {
+		return new DegreeVertexScorer();
+	}
+}
