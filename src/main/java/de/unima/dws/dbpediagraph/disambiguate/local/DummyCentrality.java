2d893567ea2e0da68f8bec6976f2f8fdd1777789
Bernhard Schäfer <bschaefer87@gmx.net>
2014-01-14 09:07:37 +0000

    new dummy disambiguator to enable usage of prior fallback in all cases

---
+++ src/main/java/de/unima/dws/dbpediagraph/disambiguate/local/DummyCentrality.java
@@ -0,0 +1,37 @@
+package de.unima.dws.dbpediagraph.disambiguate.local;
+
+import com.tinkerpop.blueprints.*;
+
+import de.unima.dws.dbpediagraph.disambiguate.*;
+import de.unima.dws.dbpediagraph.graph.GraphType;
+import de.unima.dws.dbpediagraph.model.Sense;
+import de.unima.dws.dbpediagraph.model.SurfaceForm;
+import de.unima.dws.dbpediagraph.weights.EdgeWeights;
+import edu.uci.ics.jung.algorithms.scoring.VertexScorer;
+
+/**
+ * Dummy Centrality {@link GraphDisambiguator} that assigns a score of zero to each candidate.
+ * 
+ * @author Bernhard Schäfer
+ * 
+ */
+public class DummyCentrality<T extends SurfaceForm, U extends Sense> extends AbstractLocalGraphDisambiguator<T, U>
+		implements LocalGraphDisambiguator<T, U> {
+
+	public DummyCentrality(GraphType graphType, EdgeWeights graphWeights, Boolean usePriorFallback) {
+		super(graphType, graphWeights, usePriorFallback);
+	}
+
+	private static final VertexScorer<Vertex, Double> DUMMY_SCORER = new VertexScorer<Vertex, Double>() {
+		@Override
+		public Double getVertexScore(Vertex v) {
+			return 0d;
+		}
+	};
+
+	@Override
+	protected VertexScorer<Vertex, Double> getVertexScorer(Graph subgraph) {
+		return DUMMY_SCORER;
+	}
+
+}
