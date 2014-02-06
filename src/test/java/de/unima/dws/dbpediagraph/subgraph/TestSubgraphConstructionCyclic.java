71cd143f9a82f5e9abfa2f490eb65d082c37db8b
Bernhard Schäfer <bschaefer87@gmx.net>
2014-02-06 16:25:58 +0000

    expand cyclic test case

--- src/test/java/de/unima/dws/dbpediagraph/subgraph/TestSubgraphConstructionCyclic.java
+++ src/test/java/de/unima/dws/dbpediagraph/subgraph/TestSubgraphConstructionCyclic.java
@@ -16,32 +16,38 @@ import de.unima.dws.dbpediagraph.weights.EdgeWeights;
 public class TestSubgraphConstructionCyclic {
 	private static final int MAX_DISTANCE = 10;
 	private static final EdgeWeights EDGE_WEIGHTS = DummyEdgeWeights.INSTANCE;
-	
-	private static SubgraphTester subGraphData;
+
+	private static SubgraphTester subgraphTesterDirected;
+
+	// private static SubgraphTester subgraphTesterUndirected;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException, URISyntaxException {
-		subGraphData = new SubgraphTester(TestSet.CYCLIC_FILE_NAMES, new SubgraphConstructionSettings.Builder()
-				.maxDistance(MAX_DISTANCE).build());
+		subgraphTesterDirected = new SubgraphTester(TestSet.CYCLIC_FILE_NAMES,
+				new SubgraphConstructionSettings.Builder().graphType(GraphType.DIRECTED_GRAPH)
+						.maxDistance(MAX_DISTANCE).build());
+		// subgraphTesterUndirected = new SubgraphTester(TestSet.CYCLIC_FILE_NAMES, new
+		// SubgraphConstructionSettings.Builder()
+		// .graphType(GraphType.UNDIRECTED_GRAPH).maxDistance(MAX_DISTANCE).build());
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() {
-		subGraphData.close();
+		subgraphTesterDirected.close();
 	}
 
 	@Test
 	public void testContainedEdges() {
-		for (String edgeName : subGraphData.expectedSubgraphEdges) {
-			assertNotNull("Edge should be contained in subgraph: " + edgeName,
-					subGraphData.getSubgraph().getEdge(FileUtils.lineToLabel(edgeName.split(FileUtils.DELIMITER))));
+		for (String edgeName : subgraphTesterDirected.expectedSubgraphEdges) {
+			assertNotNull("Edge should be contained in subgraph: " + edgeName, subgraphTesterDirected.getSubgraph()
+					.getEdge(FileUtils.lineToLabel(edgeName.split(FileUtils.DELIMITER))));
 		}
 	}
 
 	@Test
 	public void testContainedVertices() {
-		for (String vertexName : subGraphData.expectedSubgraphVertices) {
-			assertNotNull("Vertex should be contained in subgraph: " + vertexName, subGraphData.getSubgraph()
+		for (String vertexName : subgraphTesterDirected.expectedSubgraphVertices) {
+			assertNotNull("Vertex should be contained in subgraph: " + vertexName, subgraphTesterDirected.getSubgraph()
 					.getVertex(vertexName));
 		}
 	}
@@ -49,13 +55,14 @@ public class TestSubgraphConstructionCyclic {
 	@Test
 	public void testNumberOfEdges() {
 		double delta = 0.01;
-		assertEquals(subGraphData.expectedSubgraphEdges.size(),
-				Graphs.edgesCountWeighted(subGraphData.getSubgraph(), EDGE_WEIGHTS), delta );
+		assertEquals(subgraphTesterDirected.expectedSubgraphEdges.size(),
+				Graphs.edgesCountWeighted(subgraphTesterDirected.getSubgraph(), EDGE_WEIGHTS), delta);
 	}
 
 	@Test
 	public void testNumberOfVertices() {
-		assertEquals(subGraphData.expectedSubgraphVertices.size(), Graphs.verticesCount(subGraphData.getSubgraph()));
+		assertEquals(subgraphTesterDirected.expectedSubgraphVertices.size(),
+				Graphs.verticesCount(subgraphTesterDirected.getSubgraph()));
 	}
 
 }
