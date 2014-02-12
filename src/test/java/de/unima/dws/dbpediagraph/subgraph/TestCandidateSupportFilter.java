f36473aebf809e4ee8699e287b50537b167d0f18
Bernhard Schäfer <bschaefer87@gmx.net>
2014-02-12 15:59:09 +0000

    change bestK candidate filter from support to prior

--- src/test/java/de/unima/dws/dbpediagraph/subgraph/TestCandidateSupportFilter.java
+++ src/test/java/de/unima/dws/dbpediagraph/subgraph/TestCandidateSupportFilter.java
@@ -10,10 +10,10 @@ import de.unima.dws.dbpediagraph.model.*;
 
 public class TestCandidateSupportFilter {
 
-	private static final Sense sense1 = new DefaultSense("uri1", 0.3, 20);
-	private static final Sense sense2 = new DefaultSense("uri2", 0.2, 10);
-	private static final Sense sense3 = new DefaultSense("uri3", 0.3, 5);
-	private static final Sense sense4 = new DefaultSense("uri4", 0.3, 3);
+	private static final Sense sense1 = new DefaultSense("uri1", 0.05, 20);
+	private static final Sense sense2 = new DefaultSense("uri2", 0.05, 10);
+	private static final Sense sense3 = new DefaultSense("uri3", 0.1, 5);
+	private static final Sense sense4 = new DefaultSense("uri4", 0.2, 3);
 	private static final Sense sense5 = new DefaultSense("uri5", 0.3, 0);
 
 	private static final SurfaceForm surfaceForm1 = new DefaultSurfaceForm("sf1");
@@ -24,18 +24,18 @@ public class TestCandidateSupportFilter {
 	}
 
 	@Test
-	public void testFilterBestkSensesBySupport() {
+	public void testMaxKByPrior() {
 		Map<SurfaceForm, List<Sense>> sfssSenses = CandidateFilter.maxKByPrior(sfsSenses, 3);
 		assertEquals(1, sfssSenses.size());
 		List<Sense> best3 = sfssSenses.get(surfaceForm1);
 		assertEquals(3, best3.size());
-		assertTrue(best3.contains(sense1));
-		assertTrue(best3.contains(sense2));
 		assertTrue(best3.contains(sense3));
+		assertTrue(best3.contains(sense4));
+		assertTrue(best3.contains(sense5));
 	}
 
 	@Test
-	public void testFilterSensesBySupport() {
+	public void testByMinSupport() {
 		Map<SurfaceForm, List<Sense>> sfssSenses = CandidateFilter.byMinSupport(sfsSenses, 10);
 		assertEquals(1, sfssSenses.size());
 		List<Sense> minSupport10 = sfssSenses.get(surfaceForm1);
