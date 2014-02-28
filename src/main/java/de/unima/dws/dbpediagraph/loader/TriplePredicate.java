468245daaeb800b5ea49c462822e57a72a7bfb02
Bernhard Schäfer <bschaefer87@gmx.net>
2014-02-28 11:15:43 +0000

    add category regexp filter

--- src/main/java/de/unima/dws/dbpediagraph/loader/TriplePredicate.java
+++ src/main/java/de/unima/dws/dbpediagraph/loader/TriplePredicate.java
@@ -1,6 +1,8 @@
 package de.unima.dws.dbpediagraph.loader;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.apache.commons.configuration.Configuration;
 
@@ -65,18 +67,47 @@ enum TriplePredicate implements Predicate<Triple> {
 	 * Yield all triples where objects and subjects are not a DBpedia category.
 	 */
 	NON_CATEGORY {
-		private final String catPrefix = "http://dbpedia.org/resource/Category:";
-
 		@Override
 		public boolean apply(Triple t) {
 			// Checking for subjects is necessary since there are also triples with category subjects
 			// see e.g. topical_concepts_en.nt dump file
-			return !t.object().startsWith(catPrefix) && !t.subject().startsWith(catPrefix);
+			return !t.object().startsWith(CATEGORY_PREFIX) && !t.subject().startsWith(CATEGORY_PREFIX);
 		}
 
+	},
+	/**
+	 * Yield all triples that do not have a subject or object category matching one of the patterns.
+	 */
+	REGEXP_CATEGORY {
+		private final List<Pattern> patterns = Arrays
+				.asList(Pattern.compile("^\\d+.*$", Pattern.CASE_INSENSITIVE),
+						Pattern.compile("^.*\\d+$", Pattern.CASE_INSENSITIVE),
+						Pattern.compile("^list.*", Pattern.CASE_INSENSITIVE),
+						Pattern.compile(
+								"^.*(wiki|disambiguation|templates|portal|categories|articles|pages|redirect|navigational_boxes|infoboxes|stub).*",
+								Pattern.CASE_INSENSITIVE) //
+				);
+
+		@Override
+		public boolean apply(Triple t) {
+			return !matchesCategoryPattern(t.object()) && !matchesCategoryPattern(t.subject());
+		}
+
+		private boolean matchesCategoryPattern(String uri) {
+			if (!uri.startsWith(CATEGORY_PREFIX))
+				return false;
+			String category = uri.substring(CATEGORY_PREFIX.length());
+			for (Pattern p : patterns) {
+				if (p.matcher(category).matches())
+					return true;
+			}
+			return false;
+		}
 	};
 
 	private static final String CONFIG_TRIPLE_PREDICATE = "loading.filter.impl";
+
+	private static final String CATEGORY_PREFIX = "http://dbpedia.org/resource/Category:";
 
 	/**
 	 * Get a {@link Predicate} implementation from config.
--- src/test/java/de/unima/dws/dbpediagraph/loader/TestTriplePredicate.java
+++ src/test/java/de/unima/dws/dbpediagraph/loader/TestTriplePredicate.java
@@ -2,6 +2,7 @@ package de.unima.dws.dbpediagraph.loader;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.configuration.*;
@@ -28,6 +29,26 @@ public class TestTriplePredicate {
 	private static final Triple rdfTypeTriple = Triple.fromStringUris("http://test.org/2",
 			"http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://do.not.filter.org");
 
+	private static final List<Triple> categoryValidTriples = Arrays.asList( //
+			Triple.fromStringUris("http://dbpedia.org/resource/Ayn_Rand", "http://purl.org/dc/terms/subject",
+					"http://dbpedia.org/resource/Category:Ayn_Rand"), //
+			Triple.fromStringUris("http://dbpedia.org/resource/Ayn_Rand", "http://purl.org/dc/terms/subject",
+					"http://dbpedia.org/resource/Category:American_anti-communists"));
+
+	private static final List<Triple> categoryInvalidTriples = Arrays.asList( //
+			Triple.fromStringUris("http://dbpedia.org/resource/Ayn_Rand", "http://purl.org/dc/terms/subject",
+					"http://dbpedia.org/resource/Category:1905_births"), // "^\\d+.*$"
+			Triple.fromStringUris("http://dbpedia.org/resource/Ayn_Rand", "http://purl.org/dc/terms/subject",
+					"http://dbpedia.org/resource/Category:20th-century_philosophers"), // "^\\d+.*$"
+			Triple.fromStringUris("http://dbpedia.org/resource/Skateboarding_Hall_of_Fame",
+					"http://purl.org/dc/terms/subject",
+					"http://dbpedia.org/resource/Category:Awards_established_in_2009"), // "^.*\\d+$"
+			Triple.fromStringUris("http://dbpedia.org/resource/Abbotsford_House", "http://purl.org/dc/terms/subject",
+					"http://dbpedia.org/resource/Category:Listed_houses_in_Scotland"), // "^list.*"
+			Triple.fromStringUris("http://dbpedia.org/resource/Decision_analysis_cycle",
+					"http://purl.org/dc/terms/subject", "http://dbpedia.org/resource/Category:Decision_theory_stubs") // "^.*(wiki|disambiguation|templates|portal|categories|articles|pages|redirect|navigational_boxes|infoboxes|stub).*"
+			);
+
 	@BeforeClass
 	public static void beforeClass() throws ConfigurationException {
 		String prefix = "test-loader/testconfig";
@@ -43,6 +64,15 @@ public class TestTriplePredicate {
 		assertFalse(pred.apply(categoryObjectTriple));
 		assertTrue(pred.apply(ontologyObjectTriple));
 		assertTrue(pred.apply(rdfTypeTriple));
+	}
+
+	@Test
+	public void testRegexpCategoryPredicate() {
+		Predicate<Triple> pred = TriplePredicate.REGEXP_CATEGORY;
+		for (Triple t : categoryValidTriples)
+			assertTrue(pred.apply(t));
+		for (Triple t : categoryInvalidTriples)
+			assertFalse(pred.apply(t));
 	}
 
 	@Test
